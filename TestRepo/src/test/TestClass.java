6aba81aa32c398e7d7258e0414d9be403e188f54
Deepshikha Yadav <deepshikha.cs16@gmail.com>
2021-03-19 01:05:22 +0530

    Adding variables

--- TestRepo/src/test/TestClass.java
+++ TestRepo/src/test/TestClass.java
@@ -4,7 +4,10 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing");
+		int a = 10; 
+		int b = 10;
+		int c = a + b;
+		System.out.println("Testing" + c);
 	}
 
 }
